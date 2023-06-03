f72e0db395f71901b8418f3b4b3b8718d1e0947e
Shervin <68682979+shervinbdndev@users.noreply.github.com>
2023-06-03 20:19:47 +0330

    Commited

--- QuizApp/settings.py
+++ QuizApp/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
